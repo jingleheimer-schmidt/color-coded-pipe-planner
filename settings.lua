41faf65aca926d4709cd3b5d7681ccc81cb096a8
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-07-01 20:39:12 -0700

    add planner mode setting

--- settings.lua
+++ settings.lua
@@ -10,12 +10,24 @@
 --     }
 -- }
 
+local bots_required = {
+    type = "bool-setting",
+    name = "color-coded-pipe-planner-bots-required",
+    setting_type = "runtime-per-user",
+    default_value = true,
+    order = "a",
+}
+
+local planner_mode = {
+    type = "string-setting",
+    name = "color-coded-pipe-planner-mode",
+    setting_type = "runtime-per-user",
+    default_value = "best-guess",
+    allowed_values = { "best-guess", "perfect-match" },
+    order = "b",
+}
+
 data:extend {
-    {
-        type = "bool-setting",
-        name = "color-coded-pipe-planner-bots-required",
-        setting_type = "runtime-per-user",
-        default_value = true,
-        order = "a",
-    }
+    bots_required,
+    planner_mode,
 }
